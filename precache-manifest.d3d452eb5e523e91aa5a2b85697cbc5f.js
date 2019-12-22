2c9de5bcca02067d5c804955acf63bc54e6ec448
rizwan <rizwan.ijaz@dmechs.com>
2019-12-22 20:42:11 +0500

    Updates

---
+++ precache-manifest.d3d452eb5e523e91aa5a2b85697cbc5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c175d192dbd9c3ef65ac67659cd36f",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "1a481c8e52f19d09a7e3",
+    "url": "/contact-manager/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "0846f9102f97ebf79326",
+    "url": "/contact-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "1a481c8e52f19d09a7e3",
+    "url": "/contact-manager/static/js/2.dfdc7747.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/contact-manager/static/js/2.dfdc7747.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0846f9102f97ebf79326",
+    "url": "/contact-manager/static/js/main.5c039635.chunk.js"
+  },
+  {
+    "revision": "cafd67f10b089147bf74",
+    "url": "/contact-manager/static/js/runtime-main.450eb82b.js"
+  }
+]);
